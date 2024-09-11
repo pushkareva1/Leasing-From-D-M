4284d72d12402ee9568e7a4db1d8ae5a8a86a3c0
Pushkareva Anastasiia <laptov@1221systems.ru>
2024-09-11 16:55:16 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
+
